0e41930158c30e31cc7b37f9b67140829cf725aa
Zack Katz <zack@katz.co>
2024-08-12 05:21:30 -0400

    Add WordPress User DataSource (#7)
    * Add WP_User integration
    * Fix merging passed query with search, filters
    
    Now you can either pass a full WP_User_Query object, array, or string.
    
    Array:
    
    ```
    new WPUserDataSource( ['role__in' => ['Administrator','Editor']] );
    ```
    
    String:
    
    ```
    new WPUserDataSource( 'role=Administrator' );
    ```
    
    Searches will happen within the results set of the original query.
    
    * Add a few more field keys
    * Formatting cleanup
    * Cast User IDs to ints
    
    Static analysis was taking WordPress literally when it says it can only accept ints…
    
    * Set ID
    * Fix type tests
    * Fix fatal error wp_delete_user() not found
    
    wp_delete_user() requires user.php, which isn't loaded inside a REST request.
    
    * Restore correct sorting; it was fixed in SDK
    
    See https://github.com/UseDataKit/SDK/commit/d0c6cec6df87cd305c97ac237dc0cc80cbd11cb1
    
    * Prevent deleting your own user ID
    
    Seems like a good idea.
    
    * Remove unused imports
    * Include composer.lock for build + CI purposes
    * Code style fixes + preventing initial query.
    * Remove ignored error for CI
    * Add return types [ci skip]
    ---------
    
    Co-authored-by: Doeke Norg <doekenorg@gmail.com>

---
+++ src/Data/WPUserDataSource.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace DataKit\Plugin\Data;
+
+use DataKit\DataViews\Data\BaseDataSource;
+use DataKit\DataViews\Data\MutableDataSource;
+use DataKit\DataViews\Data\Exception\DataNotFoundException;
+use DataKit\DataViews\Data\Exception\ActionForbiddenException;
+
+/**
+ * A data source backed by WordPress users.
+ *
+ * @since $ver$
+ */
+final class WPUserDataSource extends BaseDataSource implements MutableDataSource {
+	/**
+	 * The base WP_User_Query instance that all queries will use as a starting point.
+	 *
+	 * Example: new \WP_User_Query( [ 'role' => 'editor' ] ) would be the base, and then searches will be performed
+	 * on top of that.
+	 *
+	 * @since $ver$
+	 *
+	 * @var \WP_User_Query
+	 */
+	private \WP_User_Query $base_query;
+
+	/**
+	 * Creates the data source.
+	 *
+	 * @since $ver$
+	 *
+	 * @param \WP_User_Query|array|string|null $query The WP_User_Query instance or an array of query arguments or null.
+	 */
+	public function __construct( $query = null ) {
+		if ( ! $query instanceof \WP_User_Query ) {
+			$args = $query;
+			// Prevent initial query on creation of the data source.
+			$query = new \WP_User_Query();
+			$query->prepare_query( $args );
+		}
+
+		$this->base_query = $query;
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since $ver$
+	 */
+	public function id(): string {
+		return sprintf( 'wpuser-%s', wp_hash( wp_json_encode( $this->base_query->query_vars ) ) );
+	}
+
+	/**
+	 * Merges the base query with additional query arguments.
+	 *
+	 * @since $ver$
+	 *
+	 * @param array $additional_args Additional query arguments.
+	 *
+	 * @return array The updated query.
+	 */
+	private function merge_query_vars( array $additional_args = [] ): array {
+		return array_merge( $this->base_query->query_vars, $additional_args );
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since $ver$
+	 */
+	public function get_data_ids( int $limit = 100, int $offset = 0 ): array {
+		$query = array_merge(
+			$this->get_sorting(),
+			[
+				'number' => $limit,
+				'offset' => $offset,
+				'fields' => 'ID',
+				'search' => '*' . (string) $this->search . '*',
+			],
+		);
+
+		$query      = $this->merge_query_vars( $query );
+		$user_query = new \WP_User_Query( $query );
+		$results    = $user_query->get_results();
+
+		return $results ? array_map( 'strval', $results ) : [];
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since $ver$
+	 */
+	public function get_data_by_id( string $id ): array {
+		$user = get_userdata( (int) $id );
+
+		if ( ! $user ) {
+			throw DataNotFoundException::with_id( $this, $id );
+		}
+
+		// Get all user meta data.
+		$user_meta = get_user_meta( (int) $id );
+
+		// Flatten user meta.
+		$flattened_meta = [];
+		foreach ( $user_meta as $key => $value ) {
+			$flattened_meta[ $key ] = maybe_unserialize( $value[0] );
+		}
+
+		// Combine user data with user meta.
+		return array_merge(
+			[
+				'display_name'    => $user->display_name,
+				'id'              => $user->ID,
+				'user_email'      => $user->user_email,
+				'user_login'      => $user->user_login,
+				'user_nicename'   => $user->user_nicename,
+				'user_registered' => $user->user_registered,
+				'user_status'     => $user->user_status,
+				'user_url'        => $user->user_url,
+			],
+			$flattened_meta,
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since $ver$
+	 */
+	public function count(): int {
+		$query = $this->merge_query_vars(
+			[
+				'fields'      => 'ID',
+				'count_total' => true,
+				'search'      => '*' . (string) $this->search . '*',
+			],
+		);
+
+		$user_query = new \WP_User_Query( $query );
+
+		return $user_query->get_total();
+	}
+
+	/**
+	 * Returns the sorting criteria based on the sort object.
+	 *
+	 * @since $ver$
+	 *
+	 * @return array The sorting criteria.
+	 */
+	private function get_sorting(): array {
+		if ( ! $this->sort ) {
+			return [];
+		}
+
+		$sort = $this->sort->to_array();
+
+		return [
+			'orderby' => $sort['field'],
+			'order'   => strtoupper( $sort['direction'] ),
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since $ver$
+	 */
+	public function can_delete(): bool {
+		return current_user_can( 'delete_users' );
+	}
+
+	/**
+	 * @inheritDoc
+	 * @since $ver$
+	 *
+	 * @throws DataNotFoundException    If the user does not exist before deletion.
+	 * @throws ActionForbiddenException If the current user tries to delete their own user.
+	 */
+	public function delete_data_by_id( string ...$ids ): void {
+		// wp_delete_user() requires user.php, which isn't loaded inside a REST request.
+		require_once ABSPATH . 'wp-admin/includes/user.php';
+
+		foreach ( $ids as $id ) {
+			if ( ! get_userdata( (int) $id ) ) {
+				throw DataNotFoundException::with_id( $this, $id );
+			}
+
+			if ( get_current_user_id() === (int) $id ) {
+				throw new ActionForbiddenException(
+					$this,
+					esc_html__( 'You cannot delete your own user.', 'dk-datakit' ),
+				);
+			}
+
+			wp_delete_user( (int) $id );
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since $ver$
+	 */
+	public function get_fields(): array {
+		$fields = [
+			'display_name'    => __( 'Display Name', 'dk-datakit' ),
+			'id'              => __( 'ID', 'dk-datakit' ),
+			'user_email'      => __( 'User Email', 'dk-datakit' ),
+			'user_login'      => __( 'User Login', 'dk-datakit' ),
+			'user_nicename'   => __( 'User Nicename', 'dk-datakit' ),
+			'user_registered' => __( 'User Registered', 'dk-datakit' ),
+			'user_status'     => __( 'User Status', 'dk-datakit' ),
+			'user_url'        => __( 'User URL', 'dk-datakit' ),
+		];
+
+		// Add all registered user meta keys.
+		global $wpdb;
+		$meta_keys = $wpdb->get_col( "SELECT DISTINCT meta_key FROM $wpdb->usermeta" );
+		foreach ( $meta_keys as $meta_key ) {
+			$fields[ $meta_key ] = ucfirst( str_replace( '_', ' ', $meta_key ) );
+		}
+
+		return $fields;
+	}
+}
